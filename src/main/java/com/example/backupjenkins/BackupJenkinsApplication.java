b4ddf64478511149c53bd4cccc79f76b41dc3ad2
Carlo Centofanti <carlocentos@gmail.com>
2022-03-23 15:23:16 +0100

    Initial commit

---
+++ src/main/java/com/example/backupjenkins/BackupJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backupjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackupJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backupjenkins/BackupJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backupjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
